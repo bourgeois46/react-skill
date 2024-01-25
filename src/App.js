df9c8c7c1f9f4ee71ac4972dd777520864a0049d
bourgeois <juahsome2795@gmail.com>
2024-01-25 20:12:19 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SassComponent from "./SassComponent";
+import { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
